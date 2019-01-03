555b48e4ccf0c34ba9c78c8651d85bc9a92d392a
hanlinjames90@gmail.com <hanlinjames90@gmail.com>
2019-01-02 20:36:19 -0500

    Completed 231

---
+++ 231-power-of-two/RunSolution231.java
@@ -0,0 +1,7 @@
+
+public class RunSolution231 {
+	public static void main(String[] args) {
+		System.out.println((new Solution231()).isPowerOfTwo(4));
+	}
+
+}
---
+++ 231-power-of-two/Solution231.java
@@ -0,0 +1,24 @@
+class Solution231 {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1) return true;
+        if(n == 0) return false;
+        return (n % 2 == 0 ) && isPowerOfTwo(n/2);
+    }
+    
+    public boolean isPowerOfTwoBinary(int n) {
+        if(n<=0) 
+            return false;
+     
+        while(n>2){
+            int t = n>>1;
+            int c = t<<1;
+     
+            if(n-c != 0)
+                return false;
+     
+            n = n>>1;
+        }
+     
+        return true;
+    }
+}
