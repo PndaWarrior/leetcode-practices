eabb6c8b50278f8c12e110e02ad52923bb220ce0
hanlinjames90@gmail.com <hanlinjames90@gmail.com>
2019-01-02 19:21:03 -0500

    Completed 463

---
+++ 463-island-perimeter/RunSolution463.java
@@ -0,0 +1,15 @@
+
+public class RunSolution463 {
+	public static void main(String[] args) {
+		Solution463 sol = new Solution463();
+		
+		int[][] grid = {{0,1,0,0},
+		                {1,1,1,0},
+		                {0,1,0,0},
+		                {1,1,0,0}};
+		
+		
+		System.out.println(sol.islandPerimeter(grid));
+	}
+
+}
