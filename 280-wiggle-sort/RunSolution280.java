83ff772691dcbe356fef9ce18b221c0f42857b7e
hanlinjames90@gmail.com <hanlinjames90@gmail.com>
2019-01-12 21:25:34 -0500

    Completed question 280

---
+++ 280-wiggle-sort/RunSolution280.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class RunSolution280 {
+	public static void main(String[] args) {
+		Solution280 sol = new Solution280();
+		
+		int[] nums = {3,5,2,1,6,4};
+		sol.wiggleSort(nums);
+		System.out.println(Arrays.toString(nums));
+		
+		
+	}
+
+}
