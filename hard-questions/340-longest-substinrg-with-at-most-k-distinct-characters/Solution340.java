b61b7dbc3a94a48ec4ad4590ea29712e62c63772
hanlinjames90@gmail.com <hanlinjames90@gmail.com>
2019-04-21 17:40:38 -0400

    Complete 340

--- hard-questions/340-longest-substinrg-with-at-most-k-distinct-characters/Solution340.java
+++ hard-questions/340-longest-substinrg-with-at-most-k-distinct-characters/Solution340.java
@@ -1,4 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class Solution340 {
-
+class Solution340 {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(s.length() == 0 || k == 0) return 0;
+        
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        int i = 0,
+        j = 0,
+        answer = 0;
+        
+        while(j < s.length()) {
+            char cj = s.charAt(j);
+            int count = map.getOrDefault(cj, 0);
+            map.put(cj, count + 1);
+            
+            if(map.size() <= k) {
+                answer = Math.max(answer, j-i+1);
+            }
+            
+            while(map.size() > k) {
+                char ci = s.charAt(i);
+                int ciCount = map.get(ci);
+                if(ciCount == 1) {
+                    map.remove(ci);
+                } else {
+                    map.put(ci, ciCount-1);
+                }
+                i++;
+            }
+            
+            j++;
+            
+        }
+        
+        return answer;
+    }
 }
